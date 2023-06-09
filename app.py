4d53304fe1cdafb6006b5b7852dd1568ddee84cf
Kgraph25 <muratakaito.3873@gmail.com>
2023-06-09 20:02:12 +0900

    add app.py templates/index.html static/style.css

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template, request
+from executive_model import resumate_model as remo
 
 # pylint: disable=C0103
 app = Flask(__name__)
@@ -11,6 +12,7 @@ def hello():
     if request.method == "POST":
         text = request.form["heart"]
 
+        return render_template('index.html')
     """Return a friendly HTTP greeting."""
     message = 'Welcome! Resumate!!'
 
