4d5d081159344d9c3a5d0a4a3d8cc2b0d0f76fce
Kgraph25 <muratakaito.3873@gmail.com>
2023-06-09 20:51:55 +0900

    add app.py templates/index.html static/style.css

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from executive_model import resumate_model as remo
 
 # pylint: disable=C0103
 app = Flask(__name__)
-
+a = []
 
 @app.route('/', methods=["GET", "POST"])
 def hello():
     if request.method == "POST":
         text = request.form["heart"]
         a, b, c = remo(text)
-        return render_template('index.html',key=a, name=b, vision=c)
+        return render_template('index.html',me=text, key=a, name=b, vision=c)
     else:
         message = 'Welcome! Resumate!!'
         return render_template('index.html',message=message)
