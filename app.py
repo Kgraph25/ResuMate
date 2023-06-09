7175dda4e5574de4b6cbd711111d1aed9ffb3577
Kgraph25 <muratakaito.3873@gmail.com>
2023-06-09 19:15:59 +0900

    add app.py templates/index.html static/style.css

--- app.py
+++ app.py
@@ -1,34 +1,8 @@
 import os
+
+import requests
 from flask import Flask, render_template
-import pandas as pd
-from gensim.utils import simple_preprocess as preprocess
-from gensim.models.doc2vec import Doc2Vec
 
-# CSVに基づいてデータフレームを作成
-scopes_dataframe = pd.read_csv('scopes_dataframe.csv')
-
-# キーに基づいて出力を得るためにnamesとvisionsのリストを作成する。
-names = []
-for name in scopes_dataframe['company_name']:
-    names.append(name)
-visions = []
-for vision in scopes_dataframe['scopes']:
-    visions.append(vision)
-
-# データフレームを出力
-print(scopes_dataframe)
-#モデル読み込み
-model = Doc2Vec.load('scope.model')
-# モデルを評価します。
-test_txt = input('prease_visions')
-similar_documents = model.docvecs.most_similar([model.infer_vector(preprocess(test_txt))])
-# キーを抽出します。
-keys = [document[0] for document in similar_documents]
-
-# キーを表示します。
-for key in keys:
-    # print(scopes_dataframe.loc[key])
-    print(key, names[key], ":", visions[key])
 
 # pylint: disable=C0103
 app = Flask(__name__)
@@ -36,9 +10,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    if requests.method == "POST":
+        text = requests.form("heart")
+
     """Return a friendly HTTP greeting."""
     message = 'Welcome! Resumate!!'
 
+    re
     return render_template('index.html',
 
                            message=message)
---
+++ exective_model.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from gensim.utils import simple_preprocess as preprocess
+from gensim.models.doc2vec import Doc2Vec
+
+
+def resumate_model(text):
+    # CSVに基づいてデータフレームを作成
+    scopes_dataframe = pd.read_csv('scopes_dataframe.csv')
+
+    # キーに基づいて出力を得るためにnamesとvisionsのリストを作成する。
+    names = []
+    for name in scopes_dataframe['company_name']:
+        names.append(name)
+    visions = []
+    for vision in scopes_dataframe['scopes']:
+        visions.append(vision)
+
+    # データフレームを出力
+    print(scopes_dataframe)
+    # モデル読み込み
+    model = Doc2Vec.load('scope.model')
+    # モデルを評価します。
+    test_txt = input('prease_visions')
+    similar_documents = model.docvecs.most_similar([model.infer_vector(preprocess(test_txt))])
+    # キーを抽出します。
+    keys = [document[0] for document in similar_documents]
+
+    # キーを表示します。
+    for key in keys:
+        # print(scopes_dataframe.loc[key])
+        print(key, names[key], ":", visions[key])
+
+    if __name__ == '__main__':
+        text=input('例題')
+        resumate_model(text)
