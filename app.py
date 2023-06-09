eab55cf19e150729ba2b20935ad298868f8793d7
Kgraph25 <muratakaito.3873@gmail.com>
2023-06-09 21:42:39 +0900

    編集： app.py executive_model.py

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ from executive_model import resumate_model as remo
 app = Flask(__name__)
 a = []
 
+
 @app.route('/', methods=["GET", "POST"])
 def hello():
     if request.method == "POST":
         text = request.form["heart"]
         a, b, c = remo(text)
-        return render_template('index.html',me=text, key=a, name=b, vision=c)
+        return render_template('index.html', me=text, key=a, name=b, vision=c)
     else:
         message = 'Welcome! Resumate!!'
-        return render_template('index.html',message=message)
+        return render_template('index.html', message=message)
 
 
 if __name__ == '__main__':
